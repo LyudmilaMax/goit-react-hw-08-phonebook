ed89e86ae49ca51b2ca9a463a8b5fdf501d7e91d
LyudmilaMax <109598383+LyudmilaMax@users.noreply.github.com>
2023-05-01 23:30:38 +0000

    Deploying to gh-pages from @ LyudmilaMax/goit-react-hw-08-phonebook@1882ab937289b66ef548ec28df2b1c6fb16ff0fd 🚀

---
+++ static/js/266.d2848b73.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",justifyContent:"center"},title:{fontWeight:500,fontSize:48,marginTop:50,color:"rgba(8, 53, 69, 0.88)"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Welcome to contact site"})})}}}]);
+//# sourceMappingURL=266.d2848b73.chunk.js.map
